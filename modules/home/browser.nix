6ba62277e8a8c303196bd33d7ba5ff5f768bb205
sxmchn <sxmchn.org@gmail.com>
2025-09-11 22:41:57 +0300

    Change browser

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -1,6 +1,13 @@
 { inputs, pkgs, ... }:
 {
-  home.packages = (
-    with pkgs; [ inputs.zen-browser.packages."${system}".default ]
-  );
+# home.packages = (
+#    with pkgs; [ inputs.zen-browser.packages."${system}".default ]
+# );
+  home.packages = with pkgs; [ microsoft-edge ];
+
+
+  programs.chromium = {
+    enable = true;
+    commandLineArgs = ["--enable-features=UseOzonePlatform --ozone-platform=wayland"];
+  };
 }
