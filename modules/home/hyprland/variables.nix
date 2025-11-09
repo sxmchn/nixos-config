e08c7848851aef0626e00fb0bb91eccf52f3aba4
sxmchn <sxmchn.org@gmail.com>
2025-11-09 14:52:48 +0300

    patch obsidian .desktop file add ozone platform wayland for HiDPI support

--- modules/home/hyprland/variables.nix
+++ modules/home/hyprland/variables.nix
@@ -27,5 +27,6 @@
     CLUTTER_BACKEND = "wayland";
     GTK_THEME = "Colloid-Green-Dark-Gruvbox";
     GRIMBLAST_HIDE_CURSOR = 0;
+    OBSIDIAN_USE_WAYLAND = 1;
   };
 }
--- modules/home/obsidian.nix
+++ modules/home/obsidian.nix
@@ -1,4 +1,11 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [ obsidian ];
+{config, pkgs, ...}:
+with pkgs; let
+  patchDesktop = pkg: appName: from: to: (lib.hiPrio (runCommand "$patched-desktop-entry-for-${appName}" {} ''
+  ${coreutils}/bin/mkdir -p $out/share/applications
+  ${gnused}/bin/sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop ''));
+in {
+  home.packages = with pkgs; [
+    obsidian 
+    (patchDesktop obsidian "obsidian" "^Exec=obsidian" "Exec=env OBSIDIAN_USE_WAYLAND=1 obsidian -enable-features=UseOzonePlatform -ozone-platform=wayland")
+  ];
 }
